f79bf1de01c9cc61cf8cfbbf784c19c3dafab90d
Your Name <akashmondkar03@gmail.com>
2026-06-10 20:58:13 +0530

    Folder Structure

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 
-dotenv.config();
-connectDB();
-const app = express()
 
-const port = process.env.PORT;
-const authRoutes = require("./routes/authRoutes");
 
-app.use("/", authRoutes);
-app.use(express.urlencoded({extended:false}))
-app.set("")
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+
+
+
+const app = express();
+
+
+const port=
