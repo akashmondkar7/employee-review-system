2e8f14bde008d2011fa3b1cc60a62064a1519042
Your Name <akashmondkar03@gmail.com>
2026-06-10 21:46:21 +0530

    server created

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 
-
+dotenv.config();
 
 
 
 const app = express();
 
 
-const port=
+app.get("/",(req,resp)=>{
+    resp.send("employee review system")
+})
+
+
+const port=process.env.PORT;
+app.listen(port,(req,resp)=>{
+    console.log(`Server is running on ${port}` )
+});
