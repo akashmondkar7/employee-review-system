13fea8f0f770e18f382e934bd67f6c83bd13da91
Your Name <akashmondkar03@gmail.com>
2026-06-10 20:40:04 +0530

    reset project

--- middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-export const isAdmin = (req,res,next)=>{
-    if(req.session.user.role==="admin"){
-        next();
-    }else{
-        res.send("Access Denied");
-    }
-}
