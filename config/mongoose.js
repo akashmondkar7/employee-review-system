9e26dbb627309a2596fd5449bed974176e1b77b2
Your Name <akashmondkar03@gmail.com>
2026-06-10 21:50:25 +0530

    mongo dp

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,13 @@ import  mongoose from 'mongoose'
 
 const connectDB= async ()=>{
     try {
-        await mongoose.connect(process.env.)
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDb is connected")
+
     } catch (error) {
-        
+    console.log("Database Connection Error:", error.message);
+    process.emit(1);
     }
 }
+
+export default connectDB;
