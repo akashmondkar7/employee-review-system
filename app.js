881032357d62a299bd2db4f4622235b64eaa2427
Your Name <akashmondkar03@gmail.com>
2026-06-12 18:12:24 +0530

    added register ejs

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import session from "express-session";
 import connectDB from "./config/mongoose.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/", authRoutes);
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
