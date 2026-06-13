2fcd1c4c8d4196b733974510d92fb8ae5fa626d7
akashmondkar7 <akashmondkar03@gmail.com>
2026-06-13 22:05:23 +0530

    updated admin login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -12,4 +12,33 @@ export const getEmployees = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+
+  export const getAddEmployee = (req, res) => {
+  res.render("admin/addEmployee");
+  };
+
+  export const addEmployee = async (req, res) => {
+  try {
+
+    const { name, email, password } = req.body;
+
+    const existingUser =
+      await User.findOne({ email });
+
+    if (existingUser) {
+      return res.send("User already exists");
+    }
+
+    await User.create({
+      name,
+      email,
+      password
+    });
+
+    res.redirect("/admin/employees");
+
+  } catch (error) {
+    console.log(error);
+  }
+};
 };
